dc0d3d272d92ed278d8759a292c111e4f23c7921
mayur <jadhavmayurmj53@gmail.com>
2024-09-05 04:22:53 +0530

    This project made lives little easier for workers and saved a lot of money for company by detecting false alarms our model works with 100 percent accuracy Thank you

--- false_alarm_detection.py
+++ false_alarm_detection.py
@@ -1,32 +1,37 @@
-import pickle
+
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import joblib
-from sklearn.metrics import accuracy_score
+from flask import Flask
+from flask import request
 
-df1 = pd.read_excel("C:\\Users\\HP\\Downloads\\False Alarm Cases.xlsx")
-df1.info()
-print(df1.head())
-df1.drop(['Case No.','Unnamed: 8','Unnamed: 9','Unnamed: 10'],axis=1,inplace=True)
-df1.info()
-
-
-x = df1.drop('Spuriosity Index(0/1)',axis=1)
-y = df1['Spuriosity Index(0/1)']
-
-x_train,x_test,y_train,y_test= train_test_split(x,y,test_size=0.2)
-
-lr = LogisticRegression()
-lr.fit(x_train,y_train)
-joblib.dump(lr,'FAD_data.pkl')
+app = Flask(__name__)
+@app.route('/train')
+def train():
+    df1 = pd.read_excel("C:\\Users\\HP\\Downloads\\False Alarm Cases.xlsx")
+    df1.drop(['Case No.','Unnamed: 8','Unnamed: 9','Unnamed: 10'],axis=1,inplace=True)
+    x = df1.drop('Spuriosity Index(0/1)',axis=1)
+    y = df1['Spuriosity Index(0/1)']
+    lr = LogisticRegression()
+    lr.fit(x,y)
+    joblib.dump(lr, 'FAD_data.pkl')
+    return "model trained successfully"
 
 
-lr = joblib.load('FAD_data.pkl')
 
-y_pred = lr.predict(x_test)
-print(y_pred)
+@app.route('/prediction',methods=['POST'])
+def test():
+    data = request.get_json()
+    lr = joblib.load('FAD_data.pkl')
+    y_pred = lr.predict(data,index=[0])
+    if y_pred == 0:
+        return "The Alarm is True"
+    else:
+        return "The Alarm is False"
 
-print("the accuracy of given model is ",accuracy_score(y_pred,y_test)*100,"%")
+
+
+app.run(port=8080)
+
 
